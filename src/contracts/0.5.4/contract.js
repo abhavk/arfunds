6fdfebb4db5d955403571a77ba162675e6fce1bc
Abhav Kedia <abhavkedia@Abhavs-MacBook-Pro.local>
2022-05-24 17:06:33 +0530

    added contracts

---
+++ src/contracts/0.5.4/contract.js
@@ -0,0 +1,74 @@
+
+function addOrUpdateBigStrings(object, key, qty) {
+	if (object[key]) {
+		object[key] = (BigInt(object[key]) + qty).toString();
+	} else {
+		object[key] = qty.toString();
+	}
+}
+
+function addOrUpdateIntStrings(object, key, qty) {
+	if (object[key]) {
+		object[key] = (parseInt(object[key]) + qty).toString();
+	} else {
+		object[key] = qty.toString();
+	}
+}
+
+function NumberMult(str, scalar) {
+	(Number(str)*scalar).toString();
+}
+
+export async function handle(state, action) {
+  	const balances = state.balances;
+	const caller = action.caller;
+	switch (action.input.function) {
+		case "contribute": {
+			const contribution = BigInt(SmartWeave.transaction.quantity);
+			const target = SmartWeave.transaction.target;
+			const totalSupply = parseInt(state.totalSupply);
+			const existingBalance = BigInt(await SmartWeave.getBalance(state.owner)) - contribution;
+			
+			// check inputs
+			if (target != state.owner) {
+                                throw new ContractError(`Please fund the correct owner: ${state.owner}.`);                 
+                        }
+			if (contribution == 0) {
+				throw new ContractError(`Please fund non-zero amount`);
+			}
+				
+			if (existingBalance == 0) {
+				// totalSupply==0 => existingBalance==0, but not other way round
+				// mint 100% of supply (1 M tokens)
+				state.totalSupply = "1000000";
+				state.tokens = {};
+				state.tokens[caller]="1000000";
+				
+				addOrUpdateBigStrings(state.contributors, action.caller, contribution);
+				state.totalContributions = (BigInt(state.totalContributions)+contribution).toString();
+			} else {
+                                // calculate new mints
+                                const mintedTokens = parseInt((Number(1000000000000n*contribution/existingBalance)/1000000000000) * Number(totalSupply));
+                                const adjustmentFactor = Number(totalSupply)/Number(totalSupply + mintedTokens);
+                                // Object.keys(state.tokens).map(function(key, index) {
+				var sum = 0;
+                                for (var key in state.tokens) {
+				        const newAlloc = parseInt(state.tokens[key]*adjustmentFactor);
+					sum += newAlloc;
+					state.tokens[key] = newAlloc.toString();
+                                 };
+                                 addOrUpdateIntStrings(state.tokens, action.caller, totalSupply-sum);
+                                addOrUpdateBigStrings(state.contributors, action.caller, contribution);
+                                state.totalContributions = (BigInt(state.totalContributions) + contribution).toString();
+                                
+                        } 
+			return { state };
+		}
+		default: {
+			throw new ContractError(
+				`No action ${action.input.function} exists. Please send a valid action.`
+			);
+		}
+	}
+
+}
